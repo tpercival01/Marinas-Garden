136e608c26d6db6f300a182878e270c16a289efd
tpercival01 <thomaspercival@live.co.uk>
2025-03-17 17:24:52 +0000

    Created clickable plots. Created window to create new plant.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+
+
 document.getElementById("welcome_window_button").onclick = function getName(){
     var name = document.getElementById("welcome_window_input").value;
     localStorage.setItem("name", name)
@@ -13,4 +15,16 @@ document.getElementById("instructions_window_button").onclick = function instruc
     if (temp.classList.contains("show")){
         temp.classList.remove("show");
     }
+}
+
+function addOnclicks() {
+    var elements = document.getElementsByClassName("grid_item")
+    for (var i = 0; i < elements.length; i++){
+        elements[i].onclick = function(){createPlant(this.dataset.somevalue)}
+    }
+}
+addOnclicks()
+
+function createPlant(plot_id){
+    document.getElementById("create_plant").classList.add("show");
 }
